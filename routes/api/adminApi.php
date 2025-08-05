dae61559a525381f3f12b6db1667d9584c70e18f
Ali-agela <aliagela09@gmail.com>
2025-08-05 20:59:06 +0200

    added admin routes

--- routes/api/adminApi.php
+++ routes/api/adminApi.php
@@ -1,1 +1,8 @@
+<?php
 
+use App\Http\Controllers\Admin\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/users', UserController::class);
+});
