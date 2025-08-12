65d326b2ed57f925797b955b02b7f79722f8a5f9
Ali-agela <aliagela09@gmail.com>
2025-08-13 00:41:18 +0200

    add notification management routes to adminApi for CRUD operations

--- routes/api/adminApi.php
+++ routes/api/adminApi.php
@@ -1,8 +1,15 @@
 <?php
 
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\NotificationController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
+    // User management routes
     Route::apiResource('/users', UserController::class);
+    Route::post('/users/{user}/alter-ban', [UserController::class, 'alterBan']);
+
+    // Notification management routes
+    Route::apiResource('/notifications', NotificationController::class);
+    Route::post('/notifications/{notification}/send', [NotificationController::class, 'send']);
 });
