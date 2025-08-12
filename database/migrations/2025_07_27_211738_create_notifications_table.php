9012f6ea886b697dfd95ed3a9e23b026ee50219d
Ali-agela <aliagela09@gmail.com>
2025-08-13 00:42:12 +0200

    fix migration to specify foreign key constraint for target_id in notifications table

--- database/migrations/2025_07_27_211738_create_notifications_table.php
+++ database/migrations/2025_07_27_211738_create_notifications_table.php
@@ -12,7 +12,7 @@ return new class extends Migration {
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('target_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('target_id')->nullable()->constrained('users')->onDelete('cascade');
             $table->string('en_title');
             $table->string('ar_title');
             $table->text('en_body');
