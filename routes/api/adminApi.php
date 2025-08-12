6cb09c3bf708bbedad5a0583e20f6a3fa8bdf885
Ali-agela <aliagela09@gmail.com>
2025-08-13 00:47:33 +0200

    update middleware for admin routes to include admin authentication

--- routes/api/adminApi.php
+++ routes/api/adminApi.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\NotificationController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
     // User management routes
     Route::apiResource('/users', UserController::class);
     Route::post('/users/{user}/alter-ban', [UserController::class, 'alterBan']);
