d0291abf3e2df8d7862bd1855d8804a719edab33
Ali-agela <aliagela09@gmail.com>
2025-08-12 21:03:32 +0200

    refactor User model to use UserStatusEnum for status casting

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Utility\Enums\UserStatusEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -46,13 +47,9 @@ class User extends Authenticatable implements HasMedia
      *
      * @return array<string, string>
      */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+    protected $casts = [
+        'status' => UserStatusEnum::class,
+    ];
 
     /**
      * Get the FCM tokens associated with the user.
